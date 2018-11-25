c62790b67312c7a09c67759d9ca2bc4e780e4bfb
Alejandro Romero Rivera <alejandro.romero.rivera@udc.es>
2018-11-25 10:07:13 +0100

    Model interfaces

---
+++ lib/model/gallery_storage.dart
@@ -0,0 +1,36 @@
+abstract class GalleryStorageEntry {
+  const GalleryStorageEntry(this.key);
+
+  /// String key describing identifying the entry.
+  final String key;
+
+  /// Returns a future with the image bytes with the
+  /// required level of detail [lod].
+  /// [lod] ranges between 0 - 100 with 0 the worst quality
+  /// and 100 the original quality.
+  Future<List<int>> photo(int lod);
+
+  /// Returns a future with the image bytes with the
+  /// required level of detail [lod].
+  /// /// [lod] ranges between 0 - 100 with 0 the worst quality
+  /// and 100 the original quality.
+  Future<List<int>> cartoon(int lod);
+}
+
+abstract class GalleryStorage
+{
+  /// Open this storage and allocate resources.
+  Future<void> open();
+
+  /// Closes this storage and free resources.
+  Future<void> close();
+
+  /// Find a entry by key, if not found or null returns null.
+  Future<GalleryStorageEntry> find(String key);
+
+  /// Stores an image with his cartoon equivalent in this storage.
+  Future<GalleryStorageEntry> store(List<int> photo, List<int> cartoon);
+
+  /// List this storage entries in any order.
+  Stream<GalleryStorageEntry> list();
+}
